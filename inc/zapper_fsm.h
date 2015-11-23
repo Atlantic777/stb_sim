7e89dd5ff1f8f3f81c53b1868763665eb78d4d61
Nikola Hardi <atlantic777@lugons.org>
2015-11-23 02:49:48 +0100

    zapp fsm - fsm refactoring 2/2

--- inc/zapper_fsm.h
+++ inc/zapper_fsm.h
@@ -1,6 +1,14 @@
 #ifndef ZAPPER_FSM_H
 #define ZAPPER_FSM_H
 
+#include <time.h>
+#include <signal.h>
+
+static const int WAIT_INTERVAL = 3;
+
+typedef void (*State)(char);
+typedef void(*info_callback_t)(int chan);
+
 typedef enum zapp_fsm_signal {
   SIG_NOOP,
   SIG_KEY_DOWN,
@@ -8,13 +16,32 @@ typedef enum zapp_fsm_signal {
   SIG_MAX
 } zapp_fsm_signal_t;
 
-typedef void (*State)(char);
+// timer
+static struct sigevent sev;
+static timer_t timer_id;
+static struct itimerspec fTime;
+static struct itimerspec oTime;
 
+extern State state;
+extern char input_len;
+extern char input_buff[4];
+extern int stopped;
+extern int current_chan;
+extern info_callback_t info_callback;
+
+// state handlers
 void state_idle(char ev);
 void state_num_enter(char ev);
 void state_stop(char ev);
 void state_update(char ev);
 
+// private util functions
+void schedule_update();
+int isNum(char ev);
+void process_buffer();
+void store_num(char ev);
+
+// public interface
 int  zapp_fsm_init();
 int  zapp_fsm_deinit();
 int  zapp_fsm_stopped();
--- src/zapper_fsm.c
+++ src/zapper_fsm.c
@@ -1,35 +1,13 @@
 #include "zapper_fsm.h"
-#include <stdio.h>
-#include <pthread.h>
-#include <stdlib.h>
-#include <unistd.h>
-#include <signal.h>
-#include <time.h>
-#include <sys/time.h>
-#include <errno.h>
 
-#define WAIT_INTERVAL 3
-
-static int current_chan = 1;
-typedef void(*info_callback_t)(int chan);
-
-static info_callback_t info_callback;
-
-static struct sigevent sev;
-static timer_t timer_id;
-static struct itimerspec fTime;
-static struct itimerspec oTime;
-
-static State state;
-
-char input_buff[4];
+State state;
 char input_len = 0;
+char input_buff[4];
+int stopped = 1;
+int current_chan = 1;
+info_callback_t info_callback = NULL;
 
-static int stopped;
-
-void schedule_update();
-
-static int isNum(char ev)
+int isNum(char ev)
 {
   if(ev >= '0' && ev <= '9')
   {
@@ -45,6 +23,20 @@ void process_buffer()
 {
   current_chan = atoi(input_buff);
   input_len = 0;
+}
+
+void store_num(char ev)
+{
+  input_buff[input_len] = ev;
+  input_len++;
+  input_buff[input_len] = '\0';
+
+}
+
+void schedule_update()
+{
+  state = state_update;
+  zapp_fsm_key_press(SIG_KEY_TIMEOUT);
 }
 
 int zapp_fsm_init(info_callback_t callback)
@@ -67,77 +59,9 @@ int  zapp_fsm_deinit()
   return 0;
 }
 
-void store_num(char ev)
-{
-  input_buff[input_len] = ev;
-  input_len++;
-  input_buff[input_len] = '\0';
-
-}
-
-void schedule_update(void *args)
-{
-  state = state_update;
-  zapp_fsm_key_press(SIG_KEY_TIMEOUT);
-}
-
 void zapp_fsm_key_press(char ev)
 {
   state(ev);
-}
-
-void state_idle(char ev)
-{
-  if(isNum(ev))
-  {
-    state = state_num_enter;
-    store_num(ev);
-
-    fTime.it_value.tv_sec = 2;
-    timer_settime(&timer_id, 0, &fTime, NULL);
-  }
-  else if('q' == ev)
-  {
-    state = state_stop;
-    zapp_fsm_key_press(SIG_NOOP);
-  }
-  else if('i' == ev)
-  {
-    info_callback(current_chan);
-  }
-}
-
-void state_num_enter(char ev)
-{
-  if(isNum(ev))
-  {
-    store_num(ev);
-
-    if(input_len == 3)
-    {
-      state = state_update;
-      zapp_fsm_key_press(SIG_NOOP);
-    }
-  }
-  else
-  {
-    input_len = 0;
-    state = state_idle;
-  }
-}
-
-void state_stop(char ev)
-{
-  stopped = 1;
-}
-
-void state_update(char ev)
-{
-  fTime.it_value.tv_sec = 0;
-  timer_settime(&timer_id, 0, &fTime, NULL);
-  process_buffer();
-  info_callback(current_chan);
-  state = state_idle;
 }
 
 int zapp_fsm_get_current_ch()
