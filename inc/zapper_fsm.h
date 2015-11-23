9733afa28f7abbeb8b16d9faf61eabb080553302
Nikola Hardi <atlantic777@lugons.org>
2015-11-23 02:21:56 +0100

    zapp fsm - fsm refactoring 1/2

--- inc/zapper_fsm.h
+++ inc/zapper_fsm.h
@@ -1,18 +1,26 @@
 #ifndef ZAPPER_FSM_H
 #define ZAPPER_FSM_H
 
-typedef enum zapp_fsm_state {
-  STATE_IDLE,
-  STATE_NUM_ENTER,
-  STATE_UPDATE,
-  STATE_STOP,
-} zapp_fsm_state_t;
+typedef enum zapp_fsm_signal {
+  SIG_NOOP,
+  SIG_KEY_DOWN,
+  SIG_KEY_TIMEOUT,
+  SIG_MAX
+} zapp_fsm_signal_t;
+
+typedef void (*State)(char);
+
+void state_idle(char ev);
+void state_num_enter(char ev);
+void state_stop(char ev);
+void state_update(char ev);
 
 int  zapp_fsm_init();
 int  zapp_fsm_deinit();
+int  zapp_fsm_stopped();
 int  zapp_fsm_get_current_ch();
 
 void zapp_fsm_key_press(char ev);
-int  zapp_fsm_stopped();
+
 
 #endif
--- src/zapper_fsm.c
+++ src/zapper_fsm.c
@@ -10,24 +10,22 @@
 
 #define WAIT_INTERVAL 3
 
-static int state = STATE_IDLE;
 static int current_chan = 1;
 typedef void(*info_callback_t)(int chan);
 
 static info_callback_t info_callback;
-static struct timeval last_key_press;
-static struct timespec future;
 
 static struct sigevent sev;
 static timer_t timer_id;
 static struct itimerspec fTime;
 static struct itimerspec oTime;
 
+static State state;
+
 char input_buff[4];
 char input_len = 0;
 
-static pthread_mutex_t mx = PTHREAD_MUTEX_INITIALIZER;
-static pthread_cond_t  cond = PTHREAD_COND_INITIALIZER;
+static int stopped;
 
 void schedule_update();
 
@@ -49,26 +47,23 @@ void process_buffer()
   input_len = 0;
 }
 
-int  zapp_fsm_init(info_callback_t callback)
+int zapp_fsm_init(info_callback_t callback)
 {
-  pthread_mutex_init(&mx, NULL);
-  pthread_cond_init(&cond, NULL);
-
   sev.sigev_notify = SIGEV_THREAD;
   sev.sigev_notify_function = schedule_update;
   sev.sigev_notify_attributes = NULL;
-
   timer_create(CLOCK_REALTIME, &sev, &timer_id);
-  puts("timer init");
 
+  state = state_idle;
   info_callback = callback;
+
+  stopped = 0;
+
   return 0;
 }
 
 int  zapp_fsm_deinit()
 {
-  pthread_cond_destroy(&cond);
-  pthread_mutex_destroy(&mx);
   return 0;
 }
 
@@ -80,79 +75,69 @@ void store_num(char ev)
 
 }
 
-int should_update()
-{
-  if(STATE_NUM_ENTER == state && input_len < 3)
-  {
-    return 1;
-  }
-  else
-  {
-    return 0;
-  }
-}
-
 void schedule_update(void *args)
 {
-  if(should_update())
-  {
-    puts("timer fired");
-    state = STATE_UPDATE;
-    zapp_fsm_key_press(-1);
-  }
-  else
-  {
-    puts("misfired timer");
-  }
+  state = state_update;
+  zapp_fsm_key_press(SIG_KEY_TIMEOUT);
 }
 
 void zapp_fsm_key_press(char ev)
 {
-  if(STATE_IDLE == state)
-  {
-    if(isNum(ev))
-    {
-      state = STATE_NUM_ENTER;
-      store_num(ev);
+  state(ev);
+}
 
-      fTime.it_value.tv_sec = 2;
-      timer_settime(&timer_id, 0, &fTime, NULL);
-    }
-    else if('q' == ev)
-    {
-      state = STATE_STOP;
-    }
-    else if('i' == ev)
-    {
-      info_callback(current_chan);
-    }
-  }
-  else if(STATE_NUM_ENTER == state)
+void state_idle(char ev)
+{
+  if(isNum(ev))
   {
-    if(isNum(ev))
-    {
-      store_num(ev);
+    state = state_num_enter;
+    store_num(ev);
 
-      if(input_len == 3)
-      {
-        state = STATE_UPDATE;
-        zapp_fsm_key_press(-1);
-      }
-    }
-    else
-    {
-      input_len = 0;
-      state = STATE_IDLE;
-    }
-  }
-  else if(STATE_UPDATE == state)
-  {
-    fTime.it_value.tv_sec = 0;
+    fTime.it_value.tv_sec = 2;
     timer_settime(&timer_id, 0, &fTime, NULL);
-    process_buffer();
-    info_callback(current_chan);
-    state = STATE_IDLE;
   }
+  else if('q' == ev)
+  {
+    state = state_stop;
+    zapp_fsm_key_press(SIG_NOOP);
+  }
+  else if('i' == ev)
+  {
+    info_callback(current_chan);
+  }
+}
+
+void state_num_enter(char ev)
+{
+  if(isNum(ev))
+  {
+    store_num(ev);
+
+    if(input_len == 3)
+    {
+      state = state_update;
+      zapp_fsm_key_press(SIG_NOOP);
+    }
+  }
+  else
+  {
+    input_len = 0;
+    state = state_idle;
+  }
+}
+
+void state_stop(char ev)
+{
+  stopped = 1;
+}
+
+void state_update(char ev)
+{
+  fTime.it_value.tv_sec = 0;
+  timer_settime(&timer_id, 0, &fTime, NULL);
+  process_buffer();
+  info_callback(current_chan);
+  state = state_idle;
 }
 
 int zapp_fsm_get_current_ch()
@@ -162,12 +147,5 @@ int zapp_fsm_get_current_ch()
 
 int zapp_fsm_stopped()
 {
-  if(STATE_STOP == state)
-  {
-    return 1;
-  }
-  else
-  {
-    return 0;
-  }
+  return stopped;
 }
